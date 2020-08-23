98fb5ba7190bdf29cd4e62fe57a3442b397895a4
Atishay <atishay818@gmail.com>
2020-08-23 22:37:57 +0530

    1

--- version.js
+++ version.js
@@ -1,19 +1,22 @@
-
 const options = {
-    client: 'mysql2',
-    connection: {
-        host: '127.0.0.1',
-        user: 'user12',
-        password: 's$cret',
-        database: 'mydb'
-    }
-}
+  client: "mysql2",
+  connection: {
+    host: "localhost",
+    user: "root",
+    password: "Home@127",
+    database: "curd",
+  },
+};
 
-const knex = require('knex')(options);
+const knex = require("knex")(options);
 
-knex.raw("SELECT VERSION()").then(
-    (version) => console.log((version[0][0]))
-).catch((err) => { console.log( err); throw err })
-    .finally(() => {
-        knex.destroy();
-    });
+knex
+  .raw("SELECT VERSION()")
+  .then((version) => console.log(version[0][0]))
+  .catch((err) => {
+    console.log(err);
+    throw err;
+  })
+  .finally(() => {
+    knex.destroy();
+  });
