2425dbff53d184ee75120330eb97850d5b5d3379
alphanout <47953398+alphanout@users.noreply.github.com>
2020-08-29 23:28:24 +0530

    finally

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,22 +1,26 @@
-module.exports = (app, jwt, accessTokenSecret) => {
-    // const tutorials = require("../controllers/controller.js");
+const accessTokenSecret = require("../config/auth.config").secret;
+const cont = require("../controllers/appController");
+const jwt = require("jsonwebtoken");
+
+module.exports = (app) => {
     var router = require("express").Router();
+
     const authenticateJWT = (req, res, next) => {
         const authHeader = req.headers.authorization;
-
         if (authHeader) {
             const token = authHeader.split(' ')[1];
-            
+
             jwt.verify(token, accessTokenSecret, (err, user) => {
                 if (err) {
                     return res.sendStatus(403);
                 }
-
                 req.user = user;
                 next();
             });
         } else {
-            res.sendStatus(401);
+            res.status(401).send({
+                error: "Unauthorized"
+            });
         }
     };
 
@@ -29,123 +33,50 @@ module.exports = (app, jwt, accessTokenSecret) => {
     router
         .route("/courses")
         .post(function (req, res) {
-            const fs = require("fs");
-            var data = req.body;
-            var courses = require("../../courses.json");
-            if (!Number.isInteger(data.id)) return res.send("invalid id");
-            for (let i = 0; i < courses.data.length; i++) {
-                if (courses.data[i].id == data.id) return res.send("course exist");
-            }
-            courses.data[courses.data.length] = data;
-
-            fs.writeFile("./courses.json", JSON.stringify(courses), (err) => {
-                if (err) throw err;
-                console.log("Done writing");
-            });
-            res.send("{success: true}");
+            cont.addCourse(req, res);
         })
         .get(function (req, res) {
-            var courses = require("../../courses.json");
-
-            res.send(courses.data);
+            cont.getCourses(req, res);
         });
 
     router
         .route("/courses/:courses_id")
         .get(function (req, res) {
-            var courses = require("../../courses.json");
-            var ob = req.params.courses_id;
-            if (Number.isInteger(ob)) return res.send("invalid input");
-            var id = courses.data;
-            for (let i = 0; i < id.length; i++) {
-                const element = id[i];
-                if (ob == element.id) {
-                    return res.send(element);
-                }
-            }
-            res.send("Course not found");
+            if (isNaN(+req.params.courses_id)) return res.status(400).json({
+                error: "invalid type of course id"
+            });
+            cont.getCourseById(req, res);
         });
 
     router
         .route("/students")
-        .post(function (req, res) {
-            const fs = require("fs");
-           // var data = req.body;
-            if (typeof req.body.name != typeof "") return res.send("invalid input");
-            var data={};
-            var courses = require("../../students.json");
-            data["id"] = courses.data.length;
-            data["name"]=req.body.name;
-            courses.data[courses.data.length] = data;
-
-            fs.writeFile("./students.json", JSON.stringify(courses), (err) => {
-                if (err) console.log(err);
-                console.log("Done writing");
-            });
-
-            // tutorials.create(req,res);
-            console.log("done sql");
-            res.send("{success: true}");
-        })
         .get(function (req, res) {
-            var students = require("../../students.json");
-            // console.log(courses);
-            res.send(students.data);
+            cont.getUsers(req, res);
         });
 
     router
         .route("/courses/:courses_id/enroll")
         .post(function (req, res) {
-            const fs = require("fs");
-            var data = req.body.studentId;
-            if (!Number.isInteger(data)) return res.send("invalid input");
-            var courses = require("../../courses.json");
-            var c = courses.data[req.params.courses_id - 1];
-            if (c.available_slots > 0) {
-                var p = {};
-                p["id"] = data;
-                for (let i = 0; i < c.enrolledStudents.length; i++) {
-                    if (c.enrolledStudents[i].id == data) {
-                        return res.send("Already enrolled");
-                    }
-                }
-                c.enrolledStudents.push(p);
-                c.available_slots = c.available_slots - 1;
-            } else {
-                return res.send("no slots available");
-            }
-            fs.writeFile("./courses.json", JSON.stringify(courses), (err) => {
-                if (err) throw err;
-                console.log("Done writing");
+            if (isNaN(+req.params.courses_id)) return res.status(400).json({
+                error: "invalid type of course id"
             });
-            res.send("{success: true}");
+            var data = req.body.studentId;
+            if (!Number.isInteger(data)) return res.status(400).json({
+                error: "invalid type of student id"
+            });
+            cont.enroll(req, res);
         });
 
     router
         .route("/courses/:courses_id/deregister")
         .post(function (req, res) {
-            const fs = require("fs");
-            var data = req.body.studentId;
-            if (!Number.isInteger(data)) return res.send("invalid input");
-            var courses = require("../.../../courses.json");
-            var c = courses.data[req.params.courses_id - 1];
-            var b = false;
-            for (let i = 0; i < c.enrolledStudents.length; i++) {
-                if (c.enrolledStudents[i].id == data) {
-                    c.enrolledStudents.splice(i, 1);
-                    b = true;
-                }
-            }
-            if (!b) {
-                return res.send("No student found.");
-            }
-            c.available_slots = c.available_slots + 1;
-
-            fs.writeFile("./courses.json", JSON.stringify(courses), (err) => {
-                if (err) throw err;
-                console.log("Done writing");
+            if (isNaN(+req.params.courses_id)) return res.status(400).json({
+                error: "invalid type of course id"
             });
-            res.send("{success: true}");
+            var data = req.body.studentId;
+            if (!Number.isInteger(data)) return res.send("invalid body");
+            cont.disenroll(req, res);
         });
+
     app.use("/api", authenticateJWT, router);
 };
